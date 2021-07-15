b3f92ba49905b81999ecee1a25ec7cbe09e775db
lee0507 <2eastbow@gmail.com>
2021-07-15 23:40:28 +0900

    3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function (req, res) {
-    res.send('I like kopo3030!');
+    res.send('I like kopo3030 3!');
 });
 
 app.get('/start', function (req, res) {
